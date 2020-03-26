f825d0d00774efce2f383c62856f80c651275194
Ronny Wang <ronnywang@gmail.com>
2020-03-27 00:16:47 +0800

    change to canvas

---
+++ common.js
@@ -0,0 +1,118 @@
+// From: https://mozdevs.github.io/gamedev-js-tiles/common.js
+
+var Loader = {
+    images: {}
+};
+
+Loader.loadImage = function (key, src) {
+    var img = new Image();
+
+    var d = new Promise(function (resolve, reject) {
+        img.onload = function () {
+            this.images[key] = img;
+            resolve(img);
+        }.bind(this);
+
+        img.onerror = function () {
+            reject('Could not load image: ' + src);
+        };
+    }.bind(this));
+
+    img.src = src;
+    return d;
+};
+
+Loader.getImage = function (key) {
+    return (key in this.images) ? this.images[key] : null;
+};
+
+//
+// Keyboard handler
+//
+
+var Keyboard = {};
+
+Keyboard.LEFT = 37;
+Keyboard.RIGHT = 39;
+Keyboard.UP = 38;
+Keyboard.DOWN = 40;
+
+Keyboard._keys = {};
+
+Keyboard.listenForEvents = function (keys) {
+    window.addEventListener('keydown', this._onKeyDown.bind(this));
+    window.addEventListener('keyup', this._onKeyUp.bind(this));
+
+    keys.forEach(function (key) {
+        this._keys[key] = false;
+    }.bind(this));
+}
+
+Keyboard._onKeyDown = function (event) {
+    var keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = true;
+    }
+};
+
+Keyboard._onKeyUp = function (event) {
+    var keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = false;
+    }
+};
+
+Keyboard.isDown = function (keyCode) {
+    if (!keyCode in this._keys) {
+        throw new Error('Keycode ' + keyCode + ' is not being listened to');
+    }
+    return this._keys[keyCode];
+};
+
+//
+// Game object
+//
+
+var Game = {};
+
+Game.run = function (context) {
+    this.ctx = context;
+    this._previousElapsed = 0;
+
+    var p = this.load();
+    Promise.all(p).then(function (loaded) {
+        this.init();
+        window.requestAnimationFrame(this.tick);
+    }.bind(this));
+};
+
+Game.tick = function (elapsed) {
+    window.requestAnimationFrame(this.tick);
+
+    // clear previous frame
+    this.ctx.clearRect(0, 0, 512, 512);
+
+    // compute delta time in seconds -- also cap it
+    var delta = (elapsed - this._previousElapsed) / 1000.0;
+    delta = Math.min(delta, 0.25); // maximum delta of 250 ms
+    this._previousElapsed = elapsed;
+
+    this.update(delta);
+    this.render();
+}.bind(Game);
+
+// override these methods to create the demo
+Game.init = function () {};
+Game.update = function (delta) {};
+Game.render = function () {};
+
+//
+// start up function
+//
+
+window.onload = function () {
+    var context = document.getElementById('game').getContext('2d');
+    Game.run(context);
+};
---
+++ logic-grid.js
@@ -0,0 +1,446 @@
+var map = {
+    cols: 30,
+    rows: 30,
+    tsize: 32,
+    layers: [[], []],
+    getTile: function (layer, col, row) {
+        return this.layers[layer][row * map.cols + col];
+    },
+    isSolidTileAtXY: function (x, y) {
+        var col = Math.floor(x / this.tsize);
+        var row = Math.floor(y / this.tsize);
+
+        // tiles 3 and 5 are solid -- the rest are walkable
+        // loop through all layers and return TRUE if any tile is solid
+        return this.layers.reduce(function (res, layer, index) {
+            var tile = this.getTile(index, col, row);
+            var isSolid = (tile == 'pile');
+            return res || isSolid;
+        }.bind(this), false);
+    },
+    getCol: function (x) {
+        return Math.floor(x / this.tsize);
+    },
+    getRow: function (y) {
+        return Math.floor(y / this.tsize);
+    },
+    getX: function (col) {
+        return col * this.tsize;
+    },
+    getY: function (row) {
+        return row * this.tsize;
+    }
+};
+
+var tile_map = {
+	'ground': [18,1],
+	'pile': [7,6],
+};
+for (var r = 0; r < map.rows; r ++) {
+    for (var c = 0; c < map.cols; c ++) {
+     if (c == 0 || r == 0 || c == map.cols - 1 || r == map.rows - 1) {
+         map.layers[0][r * map.cols + c] = 'ground';
+         map.layers[1][r * map.cols + c] = 'pile';
+     } else {
+         map.layers[0][r * map.cols + c] = 'ground';
+         map.layers[1][r * map.cols + c] = null;
+     }
+  }
+}
+
+
+
+function Camera(map, width, height) {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.maxX = map.cols * map.tsize - width;
+    this.maxY = map.rows * map.tsize - height;
+}
+
+Camera.prototype.follow = function (sprite) {
+    this.following = sprite;
+    sprite.screenX = 0;
+    sprite.screenY = 0;
+};
+
+Camera.prototype.update = function () {
+    // assume followed sprite should be placed at the center of the screen
+    // whenever possible
+    this.following.screenX = this.width / 2;
+    this.following.screenY = this.height / 2;
+
+    // make the camera follow the sprite
+    this.x = this.following.x - this.width / 2;
+    this.y = this.following.y - this.height / 2;
+    // clamp values
+    this.x = Math.max(0, Math.min(this.x, this.maxX));
+    this.y = Math.max(0, Math.min(this.y, this.maxY));
+
+    // in map corners, the sprite cannot be placed in the center of the screen
+    // and we have to change its screen coordinates
+
+    // left and right sides
+    if (this.following.x < this.width / 2 ||
+        this.following.x > this.maxX + this.width / 2) {
+        this.following.screenX = this.following.x - this.x;
+    }
+    // top and bottom sides
+    if (this.following.y < this.height / 2 ||
+        this.following.y > this.maxY + this.height / 2) {
+        this.following.screenY = this.following.y - this.y;
+    }
+};
+
+function Hero(map, x, y, character, name) {
+    this.map = map;
+	if (Number.isNaN(x)) {
+		x = 0;
+	}
+	if (Number.isNaN(y)) {
+		y = 0;
+	}
+    this.target_x = this.x = x;
+	this.target_y = this.y = y;
+	this.audioLevel = 0;
+    this.width = map.tsize;
+    this.height = map.tsize;
+    this.row = 0;
+    this.col = 0;
+	this.character = character;
+	this.name = name;
+	this.messages = [];
+
+	if (null !== Loader.getImage('hero:' + character)) {
+		this.image = Loader.getImage('hero:' + character);
+	} else {
+		var current_hero = this;
+		Loader.loadImage('hero:' + character, 'sprite/' + character + '.png').then(function(){
+			current_hero.image = Loader.getImage('hero:' + character);
+		});
+	}
+}
+
+Hero.SPEED = 128; // pixels per second
+
+Hero.prototype.changeCharacter = function(character) {
+	this.character = character;
+	if (null !== Loader.getImage('hero:' + character)) {
+		this.image = Loader.getImage('hero:' + character);
+	} else {
+		var current_hero = this;
+		Loader.loadImage('hero:' + character, 'sprite/' + character + '.png').then(function(){
+			current_hero.image = Loader.getImage('hero:' + character);
+		});
+	}
+};
+
+Hero.prototype.move = function (delta, dirx, diry) {
+    // move hero
+    this.x += dirx * Hero.SPEED * delta;
+    this.y += diry * Hero.SPEED * delta;
+    if (dirx || diry) {
+        this.col = (this.col + 1) % 3;
+    }
+
+    // check if we walked into a non-walkable tile
+    this._collide(dirx, diry);
+
+    // clamp values
+    var maxX = this.map.cols * this.map.tsize;
+    var maxY = this.map.rows * this.map.tsize;
+    this.x = Math.max(0, Math.min(this.x, maxX));
+    this.y = Math.max(0, Math.min(this.y, maxY));
+};
+
+Hero.prototype.otherMove = function (delta) {
+	var deltaX = deltaY = 0;
+	var row = 0;
+	if (this.target_x != this.x) {
+		var dirx = (this.target_x > this.x) ? 1 : -1;
+		row = (this.target_x > this.x) ? 2 : 1;
+		deltaX = Math.min(Hero.SPEED * delta, Math.abs(this.target_x - this.x)) * dirx;
+	} else if (this.target_y != this.y) {
+		var diry = (this.target_y > this.y) ? 1 : -1;
+		deltaY = Math.min(Hero.SPEED * delta, Math.abs(this.target_y - this.y)) * diry;
+		row = (this.target_y > this.y) ? 0 : 3;
+	}
+    // move hero
+    this.x += deltaX;
+    this.y += deltaY;
+    if (deltaX || deltaY) {
+		this.row = row;
+        this.col = (this.col + 1) % 3;
+    }
+};
+
+Hero.prototype._collide = function (dirx, diry) {
+    var row, col;
+    // -1 in right and bottom is because image ranges from 0..63
+    // and not up to 64
+    var left = this.x - this.width / 2;
+    var right = this.x + this.width / 2 - 1;
+    var top = this.y - this.height / 2;
+    var bottom = this.y + this.height / 2 - 1;
+
+    // check for collisions on sprite sides
+    var collision =
+        this.map.isSolidTileAtXY(left, top) ||
+        this.map.isSolidTileAtXY(right, top) ||
+        this.map.isSolidTileAtXY(right, bottom) ||
+        this.map.isSolidTileAtXY(left, bottom);
+    if (!collision) { return; }
+
+    if (diry > 0) {
+        row = this.map.getRow(bottom);
+        this.y = -this.height / 2 + this.map.getY(row);
+    }
+    else if (diry < 0) {
+        row = this.map.getRow(top);
+        this.y = this.height / 2 + this.map.getY(row + 1);
+    }
+    else if (dirx > 0) {
+        col = this.map.getCol(right);
+        this.x = -this.width / 2 + this.map.getX(col);
+    }
+    else if (dirx < 0) {
+        col = this.map.getCol(left);
+        this.x = this.width / 2 + this.map.getX(col + 1);
+    }
+};
+
+Game.load = function () {
+    return [
+        Loader.loadImage('tiles', 'sprite/open_tileset.png'),
+    ];
+};
+
+Game.init = function () {
+    Keyboard.listenForEvents(
+        [Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
+    this.tileAtlas = Loader.getImage('tiles');
+
+    this.heroes = {};
+    var character = $('#character').val();
+	var name = $('#name').val();
+	this.heroes.me = new Hero(map, 160, 160, character, name);
+    this.camera = new Camera(map, 512, 512);
+    this.camera.follow(this.heroes.me);
+};
+
+var prev_update_pos = null;
+Game.update = function (delta) {
+    // handle hero movement with arrow keys
+    var dirx = 0;
+    var diry = 0;
+    var row;
+    if (Keyboard.isDown(Keyboard.LEFT)) { dirx = -1; row = 1; }
+    else if (Keyboard.isDown(Keyboard.RIGHT)) { dirx = 1; row = 2; }
+    else if (Keyboard.isDown(Keyboard.UP)) { diry = -1; row = 3; }
+    else if (Keyboard.isDown(Keyboard.DOWN)) { diry = 1; row = 0; }
+    else { row = this.heroes.me.row; } 
+
+    this.heroes.me.move(delta, dirx, diry);
+	var now = (new Date).getTime();
+	for (var id in this.heroes) {
+		this.heroes[id].messages = this.heroes[id].messages.filter(function(message) {
+			return message[1] > now;
+		});
+		if (id == 'me') {
+			continue;
+		}
+		this.heroes[id].otherMove(delta);
+	}
+    this.heroes.me.row = row;
+    this.camera.update();
+
+	if (room) {
+		if (prev_update_pos === null || (new Date).getTime() - prev_update_pos > 100) {
+			room.setLocalParticipantProperty('top', this.heroes.me.y);
+			room.setLocalParticipantProperty('left', this.heroes.me.x);
+			prev_update_pos = (new Date).getTime();
+		}
+	}
+	
+};
+
+Game._drawLayer = function (layer) {
+    var startCol = Math.floor(this.camera.x / map.tsize);
+    var endCol = startCol + (this.camera.width / map.tsize);
+    var startRow = Math.floor(this.camera.y / map.tsize);
+    var endRow = startRow + (this.camera.height / map.tsize);
+    var offsetX = -this.camera.x + startCol * map.tsize;
+    var offsetY = -this.camera.y + startRow * map.tsize;
+
+    for (var c = startCol; c <= endCol; c++) {
+        for (var r = startRow; r <= endRow; r++) {
+            var tile = map.getTile(layer, c, r);
+            var x = (c - startCol) * map.tsize + offsetX;
+            var y = (r - startRow) * map.tsize + offsetY;
+            if (tile !== null && 'undefined' !== typeof(tile_map[tile])) { // 0 => empty tile
+				tileX = tile_map[tile][0];
+				tileY = tile_map[tile][1];
+                this.ctx.drawImage(
+                    this.tileAtlas, // image
+                    tileX * map.tsize, // source x
+                    tileY * map.tsize, // source y
+                    map.tsize, // source width
+                    map.tsize, // source height
+                    Math.round(x),  // target x
+                    Math.round(y), // target y
+                    map.tsize, // target width
+                    map.tsize // target height
+                );
+            }
+        }
+    }
+};
+
+Game._drawGrid = function () {
+	var width = map.cols * map.tsize;
+    var height = map.rows * map.tsize;
+    var x, y;
+    for (var r = 0; r < map.rows; r++) {
+        x = - this.camera.x;
+        y = r * map.tsize - this.camera.y;
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(width, y);
+        this.ctx.stroke();
+    }
+    for (var c = 0; c < map.cols; c++) {
+        x = c * map.tsize - this.camera.x;
+        y = - this.camera.y;
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x, height);
+        this.ctx.stroke();
+    }
+};
+
+Game._drawHeroes = function(){
+	var hero_y = [];
+	for (var id in this.heroes) {
+		hero_y.push([id, this.heroes[id].y]);
+	}
+	hero_y.sort(function(a,b) { return a[1] - b[1]; });
+	for (var id_y of hero_y) {
+		var id = id_y[0];
+		if ('undefined' === typeof(this.heroes[id].image)) {
+			continue;
+		}
+		if (id != 'me') {
+			this.heroes[id].screenX = this.heroes[id].x - Game.camera.x;
+			this.heroes[id].screenY = this.heroes[id].y - Game.camera.y;
+		}
+		this.ctx.drawImage(
+			this.heroes[id].image,
+			this.heroes[id].col * 32, this.heroes[id].row * 32, 32, 32,
+			this.heroes[id].screenX - this.heroes[id].width / 2,
+			this.heroes[id].screenY - this.heroes[id].height / 2,
+			32, 32
+		);
+
+		// audioLevel
+		this.ctx.font = '14px';
+		var textSize = this.ctx.measureText(this.heroes[id].name);
+		var textHeight = textSize.actualBoundingBoxAscent + textSize.actualBoundingBoxDescent;
+		this.ctx.fillStyle = 'rgba(255,0,0,' + this.heroes[id].audioLevel + ')';
+		
+		this.ctx.fillRect(
+			this.heroes[id].screenX - textSize.width / 2,
+			this.heroes[id].screenY - 20 - textHeight,
+			textSize.width,
+			textHeight
+		);
+
+ 		// name
+		this.ctx.textAlign = 'center';
+		this.ctx.fillStyle = 'black';
+		this.ctx.fillText(this.heroes[id].name, 
+			this.heroes[id].screenX,
+			this.heroes[id].screenY - 20
+		);
+		
+		// message
+		if (this.heroes[id].messages.length) {
+			var width = 0;
+			var height = 0;
+			metric = this.ctx.measureText(this.heroes[id].name + ':');
+			width = Math.max(width, metric.width);
+			height += metric.actualBoundingBoxAscent + metric.actualBoundingBoxDescent + 2;
+
+			for (var message of this.heroes[id].messages) {
+				metric = this.ctx.measureText(message[0]);
+				width = Math.max(width, metric.width);
+				height += metric.actualBoundingBoxAscent + metric.actualBoundingBoxDescent + 2;
+			}
+
+			this.ctx.beginPath();
+			this.ctx.fillStyle = 'white';
+			this.ctx.strokeStyle = 'black';
+			this.ctx.lineWidth = 2;
+
+			var bubbleLeft = this.heroes[id].screenX - width / 2 - 3;
+			var bubbleTop = this.heroes[id].screenY - 20 - height - 3;
+			var bubbleRight = this.heroes[id].screenX + width / 2 + 3;
+			var bubbleBottom = this.heroes[id].screenY - 20;
+
+			var radius = 2;
+			//left-top
+			this.ctx.moveTo(bubbleLeft + radius, bubbleTop);
+			//right-top
+			this.ctx.lineTo(bubbleRight - radius, bubbleTop);
+			this.ctx.quadraticCurveTo(bubbleRight, bubbleTop, bubbleRight, bubbleTop + radius);
+			//right-bottom
+			this.ctx.lineTo(bubbleRight, bubbleBottom - radius);
+			this.ctx.quadraticCurveTo(bubbleRight, bubbleBottom, bubbleRight - radius, bubbleBottom);
+			//angle
+			this.ctx.lineTo((bubbleLeft+bubbleRight)/2 + 4, bubbleBottom);
+			this.ctx.lineTo((bubbleLeft+bubbleRight)/2, bubbleBottom + 4);
+			this.ctx.lineTo((bubbleLeft+bubbleRight)/2 - 4, bubbleBottom);
+
+			//left-bottom
+			this.ctx.lineTo(bubbleLeft + radius, bubbleBottom);
+			this.ctx.quadraticCurveTo(bubbleLeft, bubbleBottom, bubbleLeft, bubbleBottom -radius);
+			// back to left-top
+			this.ctx.lineTo(bubbleLeft, bubbleTop + radius);
+			this.ctx.quadraticCurveTo(bubbleLeft, bubbleTop, bubbleLeft + radius, bubbleTop);
+
+			this.ctx.fill();
+			this.ctx.stroke();
+
+			this.ctx.textAlign = 'left';
+			this.ctx.fillStyle = 'black';
+
+			metric = this.ctx.measureText(this.heroes[id].name + ':');
+			height -= (metric.actualBoundingBoxAscent + metric.actualBoundingBoxDescent + 2);
+			this.ctx.fillText(this.heroes[id].name + ':',
+				this.heroes[id].screenX - width / 2,
+				this.heroes[id].screenY - 20 - height - 4
+			);
+			for (var message of this.heroes[id].messages) {
+				metric = this.ctx.measureText(message[0]);
+				height -= (metric.actualBoundingBoxAscent + metric.actualBoundingBoxDescent + 2);
+				this.ctx.fillText(message[0],
+					this.heroes[id].screenX - width / 2,
+					this.heroes[id].screenY - 20 - height - 4
+				);
+			}
+		}
+	}
+};
+
+Game.render = function () {
+    // draw map background layer
+    this._drawLayer(0);
+
+    // draw main character
+
+	this._drawHeroes();
+
+    // draw map top layer
+    this._drawLayer(1);
+	//this._drawGrid();
+};
